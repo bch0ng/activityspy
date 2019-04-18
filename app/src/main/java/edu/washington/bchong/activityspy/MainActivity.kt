866f7372e6729dbe88e9b6fed0a0a54847ca69b4
bch0ng <bchong@uw.edu>
2019-04-18 01:07:27 -0700

    [Init] Initializes all files

---
+++ app/src/main/java/edu/washington/bchong/activityspy/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.washington.bchong.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("Activity Spy", "onCreate event fired", Exception)
+    }
+}
